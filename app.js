dcb52691074a68c6429eeed5624bafa2b818ee5e
Elvis Estevez <124850509+ElvisAbner@users.noreply.github.com>
2023-07-04 16:01:23 +0000

    update app js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about', {
-    aboutContent: aboutContent
+    aboutStartingContent: aboutStartingContent
   });
 });
 
